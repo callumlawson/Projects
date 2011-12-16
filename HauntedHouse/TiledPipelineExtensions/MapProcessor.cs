3766f49c24ea96910010d3298f50db46e4976ee1
Callum Lawson <lawsonathome@gmail.com>
2011-12-16 21:51:46 +0000

    Added further tile support.

--- HauntedHouse/TiledPipelineExtensions/MapProcessor.cs
+++ HauntedHouse/TiledPipelineExtensions/MapProcessor.cs
@@ -5,40 +5,42 @@ using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using Microsoft.Xna.Framework.Graphics;
 using TiledLib;
+using System;
 
-namespace BasicDemoContentPipeline
+namespace HauntedHouseContentPipeline
 {
     // Each tile has a texture, source rect, and sprite effects.
     [ContentSerializerRuntimeType("HauntedHouse.Tile, HauntedHouse")]
-    public class DemoMapTileContent
+    public class MapTileContent
     {
         public ExternalReference<Texture2DContent> Texture;
         public Rectangle SourceRectangle;
         public SpriteEffects SpriteEffects;
+        public bool IsShadowCaster;
     }
 
     // For each layer, we store the size of the layer and the tiles.
     [ContentSerializerRuntimeType("HauntedHouse.Layer, HauntedHouse")]
-    public class DemoMapLayerContent
+    public class MapLayerContent
     {
         public int Width;
         public int Height;
-        public DemoMapTileContent[] Tiles;
+        public MapTileContent[] Tiles;
     }
 
     // For the map itself, we just store the size, tile size, and a list of layers.
     [ContentSerializerRuntimeType("HauntedHouse.Map, HauntedHouse")]
-    public class DemoMapContent
+    public class HauntedHouseMapContent
     {
         public int TileWidth;
         public int TileHeight;
-        public List<DemoMapLayerContent> Layers = new List<DemoMapLayerContent>();
+        public List<MapLayerContent> Layers = new List<MapLayerContent>();
     }
 
     [ContentProcessor(DisplayName = "TMX Processor - HauntedHouse")]
-    public class MapProcessor : ContentProcessor<MapContent, DemoMapContent>
+    public class MapProcessor : ContentProcessor<MapContent, HauntedHouseMapContent>
     {
-        public override DemoMapContent Process(MapContent input, ContentProcessorContext context)
+        public override HauntedHouseMapContent Process(MapContent input, ContentProcessorContext context)
         {
             // build the textures
             TiledHelpers.BuildTileSetTextures(input, context);
@@ -47,7 +49,7 @@ namespace BasicDemoContentPipeline
             TiledHelpers.GenerateTileSourceRectangles(input);
 
             // now build our output, first by just copying over some data
-            DemoMapContent output = new DemoMapContent
+            HauntedHouseMapContent output = new HauntedHouseMapContent
             {
                 TileWidth = input.TileWidth,
                 TileHeight = input.TileHeight
@@ -57,22 +59,22 @@ namespace BasicDemoContentPipeline
             foreach (LayerContent layer in input.Layers)
             {
                 // we only care about tile layers in our demo
-                TileLayerContent tlc = layer as TileLayerContent;
-                if (tlc != null)
+                TileLayerContent tileLayerContent = layer as TileLayerContent;
+                if (tileLayerContent != null)
                 {
                     // create the new layer
-                    DemoMapLayerContent outLayer = new DemoMapLayerContent
+                    MapLayerContent outLayer = new MapLayerContent
                     {
-                        Width = tlc.Width,
-                        Height = tlc.Height,
+                        Width = tileLayerContent.Width,
+                        Height = tileLayerContent.Height,
                     };
 
                     // we need to build up our tile list now
-                    outLayer.Tiles = new DemoMapTileContent[tlc.Data.Length];
-                    for (int i = 0; i < tlc.Data.Length; i++)
+                    outLayer.Tiles = new MapTileContent[tileLayerContent.Data.Length];
+                    for (int i = 0; i < tileLayerContent.Data.Length; i++)
                     {
                         // get the ID of the tile
-                        uint tileID = tlc.Data[i];
+                        uint tileID = tileLayerContent.Data[i];
 
                         // use that to get the actual index as well as the SpriteEffects
                         int tileIndex;
@@ -83,6 +85,7 @@ namespace BasicDemoContentPipeline
                         // the texture reference and source rectangle.
                         ExternalReference<Texture2DContent> textureContent = null;
                         Rectangle sourceRect = new Rectangle();
+                        bool isShadowCaster = new bool();
 
                         // iterate all the tile sets
                         foreach (var tileSet in input.TileSets)
@@ -93,6 +96,7 @@ namespace BasicDemoContentPipeline
                                 // store the texture content and source rectangle
                                 textureContent = tileSet.Texture;
                                 sourceRect = tileSet.Tiles[(int)(tileIndex - tileSet.FirstId)].Source;
+                                //isShadowCaster = Convert.ToBoolean(tileSet.Tiles[(int)(tileIndex - tileSet.FirstId)].Properties["IsShadowCaster"]);
 
                                 // and break out of the foreach loop
                                 break;
@@ -100,11 +104,12 @@ namespace BasicDemoContentPipeline
                         }
 
                         // now insert the tile into our output
-                        outLayer.Tiles[i] = new DemoMapTileContent
+                        outLayer.Tiles[i] = new MapTileContent
                         {
                             Texture = textureContent,
                             SourceRectangle = sourceRect,
-                            SpriteEffects = spriteEffects
+                            SpriteEffects = spriteEffects,
+                            IsShadowCaster = isShadowCaster
                         };
                     }
 
